2bbf9cf917753ef401c9967633279b1f99a65d4d
Cj Alexander <clauvinsalexander@gmail.com>
2023-11-07 09:26:23 -0500

    Basic skeleton made, along with functions

---
+++ script.js
@@ -0,0 +1,18 @@
+// Basic calculator functions
+const add = (first, second) => {
+    return first + second
+}
+
+const subtract = (first, second) => {
+    return first - second
+}
+
+const multiply = (first, second) => {
+    return first * second
+}
+
+const divide = (first, second) => {
+    return first / second
+}
+
+// Variables to show
